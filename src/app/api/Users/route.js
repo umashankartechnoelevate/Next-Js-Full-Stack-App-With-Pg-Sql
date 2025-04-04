c07027782e2cda2cb6a3befdd19d0371fa6c9fad
umashankartechnoelevate <umashankarjabagond@gmail.com>
2025-04-04 17:57:01 +0530

    feat:create API to insert data using next js

--- src/app/api/Users/route.js
+++ src/app/api/Users/route.js
@@ -10,3 +10,26 @@ export async function GET() {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
+
+export async function POST(req) {
+  try {
+    const body = await req.json();
+    const { name, contact } = body;
+    console.log("request body data", name);
+    console.log("request body data", contact);
+    if (!name || !contact) {
+      return NextResponse.json({ error: "name or contact required" });
+    }
+
+    const result = await query(
+      `INSERT INTO users (name,contact) VALUES($1,$2) RETURNING *`,
+      [name, contact]
+    );
+    return NextResponse.json({
+      message: "User added successfully",
+      user: result[0],
+    });
+  } catch (error) {
+    return NextResponse.json({ error: error.message, status: 500 });
+  }
+}
